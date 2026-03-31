b3d2f1b9bb972b1a1848584fbb173d61e6f13674
Tomas Piterna <piterna.tomas@seznam.cz>
2026-03-31 20:13:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,7 +1,8 @@
 /*
-This program uses basic principles of Jackson's structured programming to convert a number from the decimal to the binary number system. The program works by creating an empty string as a result beforehand, into which the converted number
-will then be written. At the beginning of the program there is a selection in case the input number is 0. Then, using loop while, the program divides the input number by two and the remainder (1 or 0) is written into the result. Then the input number is
-divided by two and rounded down. The loop repeats until the input number is 0. When that happens, the loop ends and the converted number is returned.
+This program uses basic principles of Jackson's structured programming to convert a number from the decimal to the binary number system. The program works by creating an empty string as a result beforehand,
+into which the converted number will then be written. At the beginning of the program there is a selection in case the input number is 0. Then, using while loop, the program divides the input number by two
+and the remainder (1 or 0) is written into the result. Then the input number is divided by two and rounded down. The loop repeats until the input number is 0.
+When that happens, the loop ends and the converted number is returned.
 */
 export function main(inputNumber, inputNumberSystem, outputNumberSystem) {
 let n = Number(inputNumber);
